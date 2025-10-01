14d156791ff22379d71231a20ba5442a5904232e
alejandro gutierrez yara <agutierrezyara@gmail.com>
2025-10-01 17:41:08 -0500

    cambios para usar la rama de rutas

--- app/src/main/java/com/appsnicolas/huilatravel/ui/rutas/RutasFragment.kt
+++ app/src/main/java/com/appsnicolas/huilatravel/ui/rutas/RutasFragment.kt
@@ -4,35 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.appsnicolas.huilatravel.route.SampleData
 import com.appsnicolas.huilatravel.databinding.FragmentRutasBinding
 
 class RutasFragment : Fragment() {
 
     private var _binding: FragmentRutasBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var adapter: RoutesAdapter
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val rutasViewModel =
-            ViewModelProvider(this).get(RutasViewModel::class.java)
-
         _binding = FragmentRutasBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textRutas
-        rutasViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = RoutesAdapter { route ->
         }
-        return root
+
+        binding.recyclerRutas.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerRutas.adapter = adapter
+
+        adapter.submitList(SampleData.routes)
     }
 
     override fun onDestroyView() {
