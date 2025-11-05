7a86107a9b29e590db7c089dbd45d31a317e7d73
alejandro gutierrez yara <agutierrezyara@gmail.com>
2025-11-05 02:02:52 -0500

    cambios para usar la rama de rutas y ruras intinerario

--- app/src/main/java/com/appsnicolas/huilatravel/ui/rutas/RutasFragment.kt
+++ app/src/main/java/com/appsnicolas/huilatravel/ui/rutas/RutasFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.appsnicolas.huilatravel.R
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.appsnicolas.huilatravel.route.SampleData
 import com.appsnicolas.huilatravel.databinding.FragmentRutasBinding
@@ -28,6 +31,11 @@ class RutasFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         adapter = RoutesAdapter { route ->
+            val bundle = bundleOf(
+                "routeId" to route.id,
+                "routeTitle" to route.title
+            )
+            findNavController().navigate(R.id.navigation_itinerario, bundle)
         }
 
         binding.recyclerRutas.layoutManager = LinearLayoutManager(requireContext())
