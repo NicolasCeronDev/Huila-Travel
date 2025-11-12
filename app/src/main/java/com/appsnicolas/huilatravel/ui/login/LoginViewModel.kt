b63ffb7eea0a795a1903f002c19dca99b0215519
Nicolas Cerón Castaño <nicolasceroncastano@gmail.com>
2025-11-12 17:37:21 -0500

    Favoritos (#7)
    * Delete .idea/.name
    
    * Conexion db
    
    * Login mejorado

--- app/src/main/java/com/appsnicolas/huilatravel/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/appsnicolas/huilatravel/ui/login/LoginViewModel.kt
@@ -1,31 +1,8 @@
 package com.appsnicolas.huilatravel.ui.login
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.appsnicolas.huilatravel.data.model.LoginRequest
-import com.appsnicolas.huilatravel.data.model.LoginResponse
 import com.appsnicolas.huilatravel.data.repository.LoginRepository
-import kotlinx.coroutines.launch
 
 class LoginViewModel(private val repository: LoginRepository) : ViewModel() {
-
-    private val _loginResult = MutableLiveData<Result<LoginResponse>>()
-    val loginResult: LiveData<Result<LoginResponse>> = _loginResult
-
-    fun login(correo: String, password: String) {
-        viewModelScope.launch {
-            try {
-                val response = repository.login(LoginRequest(correo, password))
-                if (response.isSuccessful && response.body() != null) {
-                    _loginResult.postValue(Result.success(response.body()!!))
-                } else {
-                    _loginResult.postValue(Result.failure(Exception(response.message())))
-                }
-            } catch (e: Exception) {
-                _loginResult.postValue(Result.failure(e))
-            }
-        }
-    }
+    // TODO: Implement the ViewModel
 }
